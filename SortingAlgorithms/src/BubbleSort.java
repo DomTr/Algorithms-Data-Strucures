dec9b534b77214a905f803f14ef8c55fd493e2ee
DomTr <61636396+DomTr@users.noreply.github.com>
2024-10-10 13:35:39 +0300

    A few comments about sorting algorithms

--- SortingAlgorithms/src/BubbleSort.java
+++ SortingAlgorithms/src/BubbleSort.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class BubbleSort {
 	// A simple Bubble Sort algorithm. Run-time O(n^2)
+	// In-place
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Length of the array: ");
--- SortingAlgorithms/src/SelectionSort.java
+++ SortingAlgorithms/src/SelectionSort.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 public class SelectionSort {
-
+	// Complexity: O(n^2)
+	// In-place
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Length of the array: ");
