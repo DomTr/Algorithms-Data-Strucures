385458350dc8b2ff3769b79306bd6db51d5131ae
DomTr <61636396+DomTr@users.noreply.github.com>
2024-11-04 09:29:49 +0200

    Improved version of QuickSort. 3 techniques to partition the array and 3 ways to choose the pivot. Works even if elements of the array are not distinct.

--- SortingAlgorithms/src/Quicksort.java
+++ SortingAlgorithms/src/Quicksort.java
@@ -1,10 +1,12 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Quicksort {
 	/*
 	 *  QuickSort algorithm:
-	 *  Runtime: O(n^2)
-	 *  Takes last element as the pivot element.
+	 *  Runtime: O(n^2) Worst-case. On average: O(n log n)
+	 *  Can take any element as the pivot element. How to choose it needs to be declared in choosePivotStrategy method
+	 *  Can partition in 3 ways: Hoare's partition, Lomuto's and the one which is not in-place.
 	 */
 	
 	public static void main(String[] args) {
@@ -28,31 +30,89 @@ public class Quicksort {
 	public static void quicksort(int[] a, int n) {
 		quicksort(a, 0, n-1);
 	}
+	public static int partitionStrategy(int[] a, int l, int r) {
+		return partitionLomuto(a, l, r);
+	}
+	static int choosePivotStrategy(int[] a, int l, int r) {
+		return choosePivotMedianFrom3(a, l, r);
+	}
 	public static void quicksort(int[] a, int l, int r) {
 		if (l < r) {
-			int k = partitionInPlace(a, l, r);
+			int k = partitionStrategy(a, l, r);
 			quicksort(a, l, k-1);
 			quicksort(a, k+1, r);
 		}
 	}
-	public static int partitionInPlace(int[] a, int l, int r) {
+	
+	static int choosePivotRand(int l, int r) {
+		// Doesn't need a[] as a parameter.
+		int interval = r-l;
+		Random rand = new Random();
+		return l+rand.nextInt(interval);
+	}
+	// Strategy for the sake of completeness,
+	static int choosePivotRight(int l, int r) {
+		return r;
+	}
+	/*
+	 *  Takes first {x} , middle {y} and the last {z} elements and returns the index of the one which is in between
+	 *  If the length is less than 3, then the right-most element is returned
+	 */ 
+	static int choosePivotMedianFrom3(int[] a, int l, int r) {
+		int interval = (r-l);
+		if (interval+1<= 2) {
+			return l+interval;
+		}
+		int x = a[l], y = a[l + interval/2], z = a[r];
+		if ((x < y && x > z) || (x > y && x < z)) {
+			return l;
+		}
+		if ((y < x && y > z) || (y > x && y < z)) {
+			return l + interval/2;
+		}
+		return r;
+	}
+	// For test case 3 doesn't work.
+	public static int partitionHoare(int[] a, int l, int r) {
 		// Find pivot and swap it to the right place
 		// Have left and right pointer. Find a suitable pair (i, j) with a[i] > p && a[j] <= p to swap with
 		int i = l;
 		int j = r-1;
-		int p = a[r];
-		while (i <= j) {
+		/* Nice trick: choosing a pivot, swapping it with the right-most element and then performing swaps
+		 * Code only for the case, when pivot is the right-most element is relevant. One can choose actually any pivot, but then do not forget it to swap with the right-most
+		 * Same trick applied in Lomuto's partition
+		 */
+		int pivotIndex = choosePivotStrategy(a, l, r);
+		int p = a[pivotIndex];
+		swap(a, pivotIndex, r);
+		
+		do {
 			while (i < r && a[i] <= p) {
 				i++;
 			}
-			while (j >= l && a[j] > p) {
+			while (j > l && a[j] > p) {
 				j--;
 			}
 			if (i < j) {
 				swap(a, i, j);
 			}
+		} while (i < j);
+		swap(a, i, r); //always swap with the right, it is the index of the pivot because we swapped at the beginning.
+		return i;
+	}
+	public static int partitionLomuto(int[] a, int l, int r) {
+		int pivotIndex = choosePivotStrategy(a, l, r);
+		int pivot = a[pivotIndex];
+		swap(a, pivotIndex, r);
+		int i = l;
+		int j;
+		for (j = l; j <= r-1; j++) {
+			if (a[j] < pivot) {
+				swap(a, i, j);
+				i++;
+			}
 		}
-		swap(a, i, r);
+		swap(a, i, j);
 		return i;
 	}
 	public static void swap(int[] a, int i, int j) {
@@ -100,5 +160,4 @@ public class Quicksort {
 		}
 		System.out.println(a[n-1]);
 	}
-
 }
