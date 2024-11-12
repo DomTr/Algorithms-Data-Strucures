3f2e284f8b1526fb5acaa4e4099ddb499537dcb7
DomTr <61636396+DomTr@users.noreply.github.com>
2024-11-12 10:45:53 +0200

    Queue implementation with linked lists

---
+++ ElementaryDataStructures/src/Queue.java
@@ -0,0 +1,40 @@
+
+public class Queue<T> {
+	private LinkedListNode<T> head;
+	private LinkedListNode<T> tail;
+	public Queue (T elem) {
+		head = new LinkedListNode<>(elem);
+		tail = head;
+	}
+	public Queue() {
+		this(null);
+	}
+	
+	public void enqueue(T elem) {
+		LinkedListNode<T> tmp = new LinkedListNode<>(elem);
+		if (head == null) {
+			head = tmp;
+			tail = head;
+			return;
+		}
+		tail.setNext(tmp);
+		tail = tmp.getNext();
+	}
+	public T dequeue() {
+		if (head == null) {
+			return null;
+		}
+		T first = head.getElem();
+		head.setNext(head.getNext());
+		return first;
+	}
+	public void appendQueue(Queue<T> q) {
+		if (head == null) {
+			head = q.head;
+			tail = q.tail;
+			return;
+		}
+		tail.setNext(q.head);
+		tail = q.tail;
+	}
+}
