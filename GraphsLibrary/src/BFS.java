ab12cae4a7e53f0100fac79ad49a877fe0c547cb
DomTr <61636396+DomTr@users.noreply.github.com>
2024-12-04 09:45:39 +0200

    Another implementation to find number of minimal paths from node u to v.

--- GraphsLibrary/src/BFS.java
+++ GraphsLibrary/src/BFS.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 public class BFS {
@@ -38,6 +39,35 @@ public class BFS {
 		return d;
 	}
 	// TODO: countMinimalPaths2 with enter and next_cut
+	int countMinimalPaths2(int n, int start, int finish, ArrayList<ArrayList<Integer>> g) {
+		int[] dp = new int[n];
+		int T = 0;
+		int[] enter = new int[n];
+		Arrays.fill(enter, -1);
+		enter[start] = T; T++;
+		int[] leave = new int[n];
+		Arrays.fill(leave, -1);
+		int nextCut = 0;
+		Queue<Integer> q = new LinkedList<>();
+		q.add(start);
+		while (!q.isEmpty()) {
+			int u = q.poll();
+			leave[u] = T; T++;
+			if (enter[u] > nextCut) {
+				nextCut = leave[u];
+			}
+			for (int w : g.get(u)) {
+				if (enter[w] == -1) {
+					q.add(w);
+					enter[w] = T; T++;
+				}
+				if (leave[w] == -1 && enter[w] > nextCut) {
+					dp[w] = dp[w] + dp[u];
+				}
+			}
+		}
+		return dp[finish];
+	}
 	// TODO: countMinimalPaths3 with while until finish is discovered
 	int countMinimalPaths1(int n, int start, int finish, ArrayList<ArrayList<Integer>> g) {
 		Queue<Integer> q = new LinkedList<>();
