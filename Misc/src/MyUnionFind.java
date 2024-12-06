66f407d0dd84b13b50b5f186d4c543c69ec25557
DomTr <61636396+DomTr@users.noreply.github.com>
2024-12-06 09:26:15 +0200

    UnionFind implementation

---
+++ Misc/src/MyUnionFind.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+
+public class MyUnionFind {
+	private int[] parent;
+	private ArrayList<ArrayList<Integer>> members;
+	public MyUnionFind (int n) {
+		parent = new int[n];
+		members = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			members.add(new ArrayList<>());
+		}
+	}
+	public void makeSet(int v) {
+		parent[v] = v;
+		members.get(v).add(v);
+	}
+	public boolean same(int u, int v) {
+		return parent[u] == parent[v];
+	}
+	public void union(int u, int v) {
+		if (members.get(u).size() > members.get(v).size()) {
+			swap(u, v);
+		}
+		for (int x : members.get(v)) {
+			parent[x] = parent[u];
+			members.get(u).add(x);
+		}
+		
+	}
+	private void swap (int x, int y) {
+		int t = x;
+		x = y;
+		y = t;
+	}
+	public static void main(String[] args) {
+		int[] a = new int[] {0, 1, 2};
+		int n = 3;
+		MyUnionFind myUnionFind = new MyUnionFind(n);
+		for (int i = 0; i < n; i++) {
+			myUnionFind.makeSet(a[i]);
+		}
+		System.out.println("First union: a[0] a[1]");
+		myUnionFind.union(a[0], a[1]);
+		System.out.println(myUnionFind.same(a[0], a[1]));
+		System.out.println(myUnionFind.same(a[1], a[0]));
+		System.out.println(myUnionFind.same(a[0], a[2]));
+		System.out.println(myUnionFind.same(a[2], a[1]));
+		System.out.println(myUnionFind.same(a[2], a[2]));
+		System.out.println("Second union: a[0], a[2]");
+		myUnionFind.union(a[0], a[2]);
+		System.out.println(myUnionFind.same(a[0], a[1]));
+		System.out.println(myUnionFind.same(a[1], a[0]));
+		System.out.println(myUnionFind.same(a[0], a[2]));
+		System.out.println(myUnionFind.same(a[2], a[1]));
+		System.out.println(myUnionFind.same(a[2], a[2]));
+	}
+}
