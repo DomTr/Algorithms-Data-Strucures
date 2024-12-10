90e4c6e2a3f81e69a624b02eb059a05291bf2433
DomTr <61636396+DomTr@users.noreply.github.com>
2024-12-10 12:39:15 +0200

    Another union-find implementation

---
+++ ElementaryDataStructures/src/UnionFind.java
@@ -0,0 +1,29 @@
+
+public class UnionFind {
+	private int[] parent;
+	public UnionFind(int n) {
+		parent = new int[n];
+		for (int i = 0; i < n; i++) {
+			parent[i] = i;
+		}
+	}
+	public void makeSet(int u) {
+		parent[u] = u;
+	}
+	public int findSet(int u) {
+		if (parent[u] == u) {
+			return u;
+		} else {
+			parent[u] = findSet(parent[u]);
+			return parent[u];
+		}
+	}
+	void unionSets(int u, int v) {
+		int a = findSet(u);
+		int b = findSet(v);
+		if (a != b) {
+			parent[b] = a;
+		}
+	}
+	
+}
