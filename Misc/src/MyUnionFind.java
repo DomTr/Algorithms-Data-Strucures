a5c828fd6df792d940b7873b59293b294a35f09c
DomTr <61636396+DomTr@users.noreply.github.com>
2024-12-13 09:01:15 +0200

    Corrected a mistake, added an additional attribute in MyUnionFind

--- Misc/src/MyUnionFind.java
+++ Misc/src/MyUnionFind.java
@@ -2,42 +2,53 @@ import java.util.ArrayList;
 
 public class MyUnionFind {
 	private int[] parent;
+	public int biggestComponent;
 	private ArrayList<ArrayList<Integer>> members;
-	public MyUnionFind (int n) {
+
+	public MyUnionFind(int n) {
 		parent = new int[n];
 		members = new ArrayList<>();
 		for (int i = 0; i < n; i++) {
 			members.add(new ArrayList<>());
 		}
+		for (int i = 0; i < n; i++) {
+			makeSet(i);
+		}
+		biggestComponent = 1;
 	}
+
 	public void makeSet(int v) {
 		parent[v] = v;
 		members.get(v).add(v);
 	}
+
 	public boolean same(int u, int v) {
 		return parent[u] == parent[v];
 	}
-	// Complexity: O(min(|ZHK[u]|, |ZHK[v]|)), where ZHK[i] is the connected component
+
+	// Complexity: O(min(|ZHK[u]|, |ZHK[v]|)), where ZHK[i] is the connected
+	// component
 	// If it is done on the whole set, the amortized complexity is O(n log n)
-	// if ZHK'(u) is the new connected component, |ZHK'[u]| = |ZHK[u]| + |ZHK[v]| >= |ZHK[u]| + |ZHK[u]| = 2|ZHK[u]| 
+	// if ZHK'(u) is the new connected component, |ZHK'[u]| = |ZHK[u]| + |ZHK[v]| >=
+	// |ZHK[u]| + |ZHK[u]| = 2|ZHK[u]|
 	public void union(int u, int v) {
-		if (members.get(u).size() > members.get(v).size()) {
-			swap(u, v);
+		if (members.get(parent[u]).size() > members.get(parent[v]).size()) {
+			int t = u;
+			u = v;
+			v = t;
 		}
 		// merge the smaller set to the bigger one. |ZHK[u]| >= |ZHK[v]|
 		for (int x : members.get(u)) {
 			parent[x] = parent[v];
-			members.get(v).add(x);
+			members.get(parent[v]).add(x);
 		}
-		
+		biggestComponent = Math.max(biggestComponent, members.get(parent[v]).size());
+
 	}
-	private void swap (int x, int y) {
-		int t = x;
-		x = y;
-		y = t;
-	}
+
+
 	public static void main(String[] args) {
-		int[] a = new int[] {0, 1, 2};
+		int[] a = new int[] { 0, 1, 2 };
 		int n = 3;
 		MyUnionFind myUnionFind = new MyUnionFind(n);
 		for (int i = 0; i < n; i++) {
