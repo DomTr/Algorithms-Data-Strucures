4e05a319703f643a733421f062028dc0903ab94e
DomTr <61636396+DomTr@users.noreply.github.com>
2025-01-03 13:04:36 +0200

    UnionFind

--- ElementaryDataStructures/src/UnionFind.java
+++ ElementaryDataStructures/src/UnionFind.java
@@ -1,11 +1,13 @@
 
 public class UnionFind {
 	private int[] parent;
-
+	private int[] rank;
 	public UnionFind(int n) {
 		parent = new int[n];
+		rank = new int[n];
 		for (int i = 0; i < n; i++) {
 			parent[i] = i;
+			rank[i] = 0;
 		}
 	}
 
@@ -25,8 +27,13 @@ public class UnionFind {
 	void unionSets(int u, int v) {
 		int a = findSet(u);
 		int b = findSet(v);
-		if (a != b) {
+		if (rank[a] < rank[b]) {
+			parent[a] = parent[b];
+		} else if (rank[a] > rank[b]) {
+			parent[b] = parent[a];
+		} else {
 			parent[b] = a;
+			rank[a]++;
 		}
 	}
 
