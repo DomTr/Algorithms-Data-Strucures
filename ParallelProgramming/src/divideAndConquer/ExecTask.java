eac0e2aa38195091025feea5f3e17f87495c01e3
DomTr <61636396+DomTr@users.noreply.github.com>
2025-03-17 10:07:56 +0200

    ExecutorService Example

---
+++ ParallelProgramming/src/divideAndConquer/ExecTask.java
@@ -0,0 +1,61 @@
+package divideAndConquer;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+// Finds minimal
+public class ExecTask implements Callable<Integer>{
+	int l, h;
+	int[] arr;
+	ExecutorService ex;
+	
+	public ExecTask(ExecutorService ex, int l, int h, int[] arr) {
+		this.ex = ex;
+		this.l = l;
+		this.h = h;
+		this.arr = arr;
+	}
+	public Integer call() throws Exception {
+        //System.out.println("In call " + Thread.currentThread().getName() + " for range [" + l + ", " + h + ")");
+        
+        // Base case
+        if (h - l <= 2) { // if BC is h - l == 1 and number of threads is 4, then will not terminate
+            return h - l == 1 ? arr[l] : Math.min(arr[l],arr[l+1]);
+        }
+
+        int mid = l + (h - l) / 2; 
+        //sSystem.out.println(mid);
+        ExecTask t1 = new ExecTask(ex, l, mid, arr);
+        ExecTask t2 = new ExecTask(ex, mid, h, arr);
+
+        // Submit tasks for parallel execution
+        Future<Integer> f1 = ex.submit(t1);
+        Future<Integer> f2 = ex.submit(t2);
+
+        try {
+            // Combine the results
+            return Math.min(f1.get(), f2.get());
+        } catch (Exception e) {
+            e.printStackTrace();  // Print stack trace to debug
+            return Integer.MAX_VALUE; // Return a large value to indicate an error
+        }
+    }
+	public static void main(String[] args) {
+		int[] arr = new int[] {15, 12, 99, 89, 1, -10, 29, -100};
+		//ExecutorService ex = Executors.newFixedThreadPool(10); // If not enough threads this will not terminate
+		ExecutorService ex = Executors.newCachedThreadPool();
+
+		ExecTask task = new ExecTask(ex, 0, arr.length, arr);
+		Future<Integer> min = ex.submit(task);
+		try {
+			System.out.println(min.get());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			ex.shutdown();
+		}
+
+	}
+}
