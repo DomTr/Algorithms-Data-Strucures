a2452ce22c4b42d548fdb0e6b8801cc44115124d
DomTr <61636396+DomTr@users.noreply.github.com>
2025-04-10 10:32:08 +0300

    Get size, biggest, smallest element of a set in DSU functionality.

--- ElementaryDataStructures/src/UnionFind.java
+++ ElementaryDataStructures/src/UnionFind.java
@@ -2,11 +2,21 @@
 public class UnionFind {
 	private int[] parent;
 	private int[] rank;
+	private int[] smallestPar;
+	private int[] biggestPar;
+	private int[] size;
+
 	public UnionFind(int n) {
 		parent = new int[n];
 		rank = new int[n];
+		smallestPar = new int[n];
+		biggestPar = new int[n];
+		size = new int[n];
 		for (int i = 0; i < n; i++) {
 			parent[i] = i;
+			smallestPar[i] = i;
+			biggestPar[i] = i;
+			size[i] = 1;
 			rank[i] = 0;
 		}
 	}
@@ -15,26 +25,40 @@ public class UnionFind {
 		parent[u] = u;
 	}
 
-	public int findSet(int u) {
-		if (parent[u] == u) {
-			return u;
+	public int findSet(int x) {
+        if (biggestPar[x] == x) return x;
+        else {
+            int p = findSet(biggestPar[x]);
+            biggestPar[x] = p;
+            return p;
+        }
+    }
+
+	public void union(int x, int y) {
+
+		int parX = findSet(x), parY = findSet(y);
+		if (parX == parY)
+			return;
+		if (parX > parY) {
+			size[parX] += size[parY];
+			biggestPar[parY] = parX;
+			smallestPar[parX] = Math.min(smallestPar[parX], smallestPar[parY]);
 		} else {
-			parent[u] = findSet(parent[u]);
-			return parent[u];
+			size[parY] += size[parX];
+			biggestPar[parX] = parY;
+			smallestPar[parY] = Math.min(smallestPar[parX], smallestPar[parY]);
 		}
 	}
 
-	void unionSets(int u, int v) {
-		int a = findSet(u);
-		int b = findSet(v);
-		if (rank[a] < rank[b]) {
-			parent[a] = parent[b];
-		} else if (rank[a] > rank[b]) {
-			parent[b] = parent[a];
-		} else {
-			parent[b] = a;
-			rank[a]++;
-		}
+	public int getSize(int x) {
+		return size[findSet(x)];
 	}
 
+	public int getMin(int x) {
+		return smallestPar[findSet(x)];
+	}
+
+	public int getMax(int x) {
+		return biggestPar[findSet(x)];
+	}
 }
