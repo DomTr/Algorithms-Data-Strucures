bf845383c1c9dd18e62000e69578a6e9d39ca92a
DomTr <61636396+DomTr@users.noreply.github.com>
2025-06-03 20:11:15 +0200

    Extended ternary search for golden section

--- BinarySearch/src/TernarySearch.java
+++ BinarySearch/src/TernarySearch.java
@@ -8,6 +8,7 @@ public class TernarySearch {
 	public static int ternarySearch(int[] array, int x) {
 		return ternarySearch(array, x, 0, array.length - 1);
 	}
+	
 	private static int ternarySearch(int[] array, int x, int l, int r) {
 		if (l > r)
 			return -1;
@@ -26,5 +27,28 @@ public class TernarySearch {
 			return ternarySearch(array, x, pivotRight + 1, r);
 		}
 		return ternarySearch(array, x, pivotLeft + 1, pivotRight - 1);
+	}
+	/*
+	 * One of the two must apply
+	 * 1. f strictly increases, reaches its maximum and starts decreasing
+	 * 2. f strictly decreases, reaches its minimum and starts increasing
+	 * Then ternary search is applicable for searching for the golden section
+	 */
+	static double f(double x) {
+		return 0;
+	}
+	public static double ternarySearch(double l, double r) {
+		for (int it = 1; it <= 100; it++) { // could be replaced with while(r - l > eps), where eps = 1e-6
+			double m1 = l + (r-l)/3;
+			double m2 = r - (r-l)/3;
+			double f1 = f(m1);
+			double f2 = f(m2);
+			if (f1 < f2) {
+				l = m1;
+			} else {
+				r = m2;
+			}
+		}
+		return f(l);
 	}
 }
